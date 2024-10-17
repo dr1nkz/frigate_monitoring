98fa9ac5ce51eb959410960cbc60b92226740485
dr1nkz <raptorpower1@gmail.com>
2024-10-17 13:53:20 +0300

    isolated frigate

--- mqtt/script.py
+++
@@ -1,7 +0,0 @@
-import torch
-import onnxruntime
-
-print(onnxruntime.get_available_providers())
-providers = ['CUDAExecutionProvider', 'CPUExecutionProvider']
-path = '/mqtt/speed_estimation/yolov9c.onnx'
-session = onnxruntime.InferenceSession(path, providers=providers)
