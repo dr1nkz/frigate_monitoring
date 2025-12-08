372dc9633d43d0e9622f0ea6c76410fe027c8e87
dr1nkz <raptorpower1@gmail.com>
2025-12-09 00:56:21 +0300

    mqtt/demo_notificator.py fix

--- mqtt/demo_notificator.py
+++ mqtt/demo_notificator.py
@@ -2,6 +2,7 @@ import os
 import base64
 import json
 import boto3
+import time
 import paho.mqtt.client as mqtt
 
 # ==== S3 settings ====
@@ -15,6 +16,7 @@ S3_BUCKET = os.getenv('S3_BUCKET', 'test')
 MQTT_BROKER = os.getenv('MQTT_BROKER', '192.168.16.21')
 MQTT_PORT = int(os.getenv('MQTT_PORT', 1883))
 MQTT_TOPIC = os.getenv('MQTT_TOPIC', 'violations')
+CLIENT_ID = os.getenv('CLIENT_ID', 'mqtt_events_listener-0')
 
 
 def upload_file_to_s3(file_path, bucket_name, object_key):
@@ -33,32 +35,49 @@ def upload_file_to_s3(file_path, bucket_name, object_key):
         return None
 
 
-def send_mqtt_message(filename, start_time, end_time, median_speed, max_detected_speed, duration):
+def send_mqtt_message(filename, start_time, end_time, median_speed, max_detected_speed, duration, uploaded_files, description):
     # Формируем payload для MQTT
+
+    #    payload = {
+    #        'title': 'Демо нарушение',
+    #        'start_time': start_time,
+    #        'end_time': end_time,
+    #        'median_speed': median_speed,
+    #        'max_detected_speed': max_detected_speed,
+    #        'duration': duration,
+    #        'error_type_codes': [1],
+    #        'description': 'Демо отправка через скрипт',
+    #        'source_data': 'demo_script',
+    #        'extra_data': {'demo': True},
+    #        'file': filename,
+    #    }
     payload = {
         'title': 'Демо нарушение',
-        'start_time': start_time,
-        'end_time': end_time,
-        'median_speed': median_speed,
+        'dataAt': start_time,
+        'dataEnd': end_time,
+        'speed': median_speed,
         'max_detected_speed': max_detected_speed,
         'duration': duration,
         'error_type_codes': [1],
-        'description': 'Демо отправка через скрипт',
+        'description': description,
         'source_data': 'demo_script',
         'extra_data': {'demo': True},
-        'file': filename,
+        'files': uploaded_files,
     }
 
     # Отправляем в MQTT
-    client = mqtt.Client(callback_api_version=5)
+    # client = mqtt.Client(callback_api_version=5)
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, CLIENT_ID)
     client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    client.publish(MQTT_TOPIC, json.dumps(payload))
+    client.publish(MQTT_TOPIC, json.dumps(payload), qos=2)
     print(payload)
     client.disconnect()
     print(f"Данные отправлены в MQTT топик {MQTT_TOPIC}")
 
 
 def upload_file_to_s3_and_send_mqtt_message(filename, directory, start_time, end_time, median_speed, max_detected_speed, duration):
+    uploaded_files = []
+    description = 'Нарушение скорости-'+time.strftime("%H:%M:%S %d-%m-%Y")
     file_path = os.path.join(directory, filename)
     if not os.path.exists(file_path):
         print(f"Файл не найден: {file_path}")
@@ -67,8 +86,15 @@ def upload_file_to_s3_and_send_mqtt_message(filename, directory, start_time, end
     result_key = upload_file_to_s3(file_path, S3_BUCKET, s3_key)
     if result_key:
         print(f"Загружено: {filename} -> {result_key}")
+        uploaded_files.append({
+            'file_name': filename,
+            'file_comment': 'Видео-'+time.strftime("%Y%m%d-%H%M%S"),
+            's3_key': result_key,
+        })
     else:
         print(f"Ошибка загрузки: {filename}")
-
-    send_mqtt_message(filename, start_time, end_time,
-                      median_speed, max_detected_speed, duration)
+    if not uploaded_files:
+        print("Нет файлов для отправки в MQTT")
+        return
+    send_mqtt_message(filename, start_time, end_time, median_speed,
+                      max_detected_speed, duration, uploaded_files, description)
